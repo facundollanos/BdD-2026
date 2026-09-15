ae2cb886cfc014e8f12b0c8812a522a21fb14934
Facundo Llanos <76456958+facundollanos@users.noreply.github.com>
2026-09-15 20:23:43 -0300

    update ejercicio_18.sql

--- guiaDeEjercicios/ejercicio_18.sql
+++ guiaDeEjercicios/ejercicio_18.sql
@@ -1,6 +1,5 @@
 /*
 
-EMPEZANDOLO SIN TERMINAR
 
 
 18. Escriba una consulta que retorne una estadística de ventas para todos los rubros.
@@ -16,20 +15,34 @@ por cantidad de productos diferentes vendidos del rubro.
 */
 
 
-
-select 
-	R.rubr_detalle as DetalleRubro,
-	(
-		Select Sum(I2.item_cantidad * I2.item_precio) from Item_Factura I2 
-			JOIN Producto P2 ON I2.item_producto = P2.prod_codigo
-				where I2.item_tipo+I2.item_sucursal+I2.item_numero = F.fact_tipo+F.fact_sucursal+F.fact_numero AND
-					  P2.prod_rubro = R.rubr_id			
-	) AS VENTAS_EN_PESOS_RUBRO,
-	(SELECT TOP 1 COUNT(F3.fact_cliente) FROM Factura F3 
-		JOIN Item_Factura I3 ON
-	GROUP BY F3.fact_cliente )
-		FROM Rubro R 
-		LEFT JOIN Producto P ON P.prod_rubro = R.rubr_id
-		RIGHT JOIN Item_Factura I ON I.item_producto = P.prod_codigo
-		JOIN Factura F ON F.fact_tipo+F.fact_sucursal+F.fact_numero=I.item_tipo+I.item_sucursal+I.item_numero
-		GROUP BY R.rubr_id,R.rubr_detalle , F.fact_tipo,F.fact_sucursal,F.fact_numero
+SELECT r.rubr_detalle, 
+(SELECT SUM(i2.item_cantidad * i2.item_precio)
+FROM Item_Factura i2
+INNER JOIN Producto p2 ON i2.item_producto = p2.prod_codigo
+WHERE p2.prod_rubro = r.rubr_id) AS Ventas, 
+i.item_producto AS MasVendido,
+ISNULL((SELECT TOP 1 p2.prod_codigo
+FROM Item_Factura i2
+INNER JOIN Producto p2 ON i2.item_producto = p2.prod_codigo
+WHERE p2.prod_rubro = r.rubr_id AND p2.prod_codigo != i.item_producto
+GROUP BY p2.prod_codigo 
+ORDER BY SUM(i2.item_cantidad) DESC) , '00000000') AS SegundoMasVendido,
+ISNULL((SELECT TOP 1 F.fact_cliente FROM Factura F 
+INNER JOIN Item_Factura I ON F.fact_numero = I.item_numero AND F.fact_sucursal = I.item_sucursal AND F.fact_tipo = I.item_tipo 
+INNER JOIN Producto P ON i.item_producto = p.prod_codigo 
+WHERE DATEDIFF(DAY,F.fact_fecha,getdate()) < 31 AND 
+P.prod_rubro = R.rubr_id
+GROUP BY F.fact_cliente 
+ORDER BY SUM(I.item_cantidad) DESC
+),0) AS Cliente
+FROM Item_Factura i
+INNER JOIN Producto p ON i.item_producto = p.prod_codigo 
+INNER JOIN Rubro r ON p.prod_rubro = r.rubr_id
+GROUP BY r.rubr_id, r.rubr_detalle, i.item_producto  
+HAVING I.item_producto = (
+SELECT TOP 1 p2.prod_codigo
+FROM Item_Factura i2
+INNER JOIN Producto p2 ON i2.item_producto = p2.prod_codigo
+WHERE p2.prod_rubro = r.rubr_id
+GROUP BY p2.prod_codigo 
+ORDER BY SUM(i2.item_cantidad) DESC)
