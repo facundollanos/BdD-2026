7ec7c8bf63bea1467962c507352ae4b4956bc3a7
Facundo Llanos <76456958+facundollanos@users.noreply.github.com>
2026-09-22 19:39:56 -0300

    update ejercicio_33.sql
    Added SQL query to calculate average price and total sold for products in 2012.

--- guiaDeEjercicios/ejercicio_33.sql
+++ guiaDeEjercicios/ejercicio_33.sql
@@ -10,6 +10,33 @@
 -- f. Total facturado para ese producto
 -- El resultado deberá ser ordenado por el total vendido por producto para el año 2012.
 
+
+--solucion profesor
+
+SELECT P.prod_codigo, P.prod_detalle, SUM(i.item_cantidad) AS CantidadUnidades,
+COUNT(*) AS cantidadFacturas,
+SUM(i.item_cantidad * i.item_precio) / SUM(i.item_cantidad) AS PrecioPromedio,
+SUM(i.item_cantidad * i.item_precio) AS totalFacturado
+FROM 
+Composicion c
+INNER JOIN producto p ON c.comp_componente = p.prod_codigo
+INNER JOIN Item_Factura i ON p.prod_codigo = i.item_producto 
+WHERE c.comp_producto IN (
+SELECT TOP 1 i.item_producto
+FROM Composicion c
+INNER JOIN Item_Factura i ON c.comp_producto = i.item_producto
+INNER JOIN Factura f ON i.item_tipo = f.fact_tipo AND i.item_sucursal = f.fact_sucursal AND i.item_numero = f.fact_numero
+WHERE YEAR(f.fact_fecha) = 2012
+GROUP BY i.item_producto 
+ORDER BY SUM(i.item_cantidad) DESC)
+GROUP BY P.prod_codigo, P.prod_detalle 
+ORDER BY (SELECT SUM(i2.item_cantidad * i2.item_precio)
+from Factura f2 
+INNER JOIN item_factura i2 
+ON i2.item_tipo = f2.fact_tipo AND i2.item_sucursal = f2.fact_sucursal AND i2.item_numero = f2.fact_numero 
+WHERE YEAR(f2.fact_fecha) = 2012
+AND i2.item_producto = P.prod_codigo) DESC
+
 SELECT
     p.prod_codigo,
 
